996e25060fe385a313206fff543cc1851cd00748
JalsiraSolino <jalsirasolino1@gmail.com>
2024-06-19 22:19:06 +0000

    front Alterado

--- paginas/cadastro.php
+++ paginas/cadastro.php
@@ -1,16 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cadastrar Aluno</title>
+    <link rel="stylesheet" href="../css/menu.css">
+    <title>Cadastrar Aluno</title>    
 </head>
 <body>
     <h2>Cadastrar Aluno</h2>
-    <form action="cadastro.php" method="POST">
-        Nome: <input type="text" name="nome" required><br>
-        Email: <input type="email" name="email" required><br>
-        Telefone: <input type="text" name="telefone" required><br>
-        <input type="submit" value="Cadastrar">
-    </form>
+    <div class="box">
+        <form action="cadastro.php" method="POST">
+            
+                <div class="boxform">
+                    <input type="text" name="nome" id="nome" required class="inputLabel">
+                    <label for="nome" class="labelinput"> Nome completo</label><br><br>
+                </div>
+                
+                <div class="boxform">
+                    <input type="email" name="email" id="email" class="inputLabel" required>
+                    <label for="email" class="labelinput">Email</label><br><br>
+                </div>
+                
+                <div class="boxform">
+                    <input type="text" name="telefone" id="telefone" placeholder="9XX XXX XXX" class="inputLabel">
+                    <label for="cidade" class="labelinput">Telefone</label> 
+                </div> 
+                <input type="submit" id="submit" value="Cadastrar">
+        </form>
+    </div>
     
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
